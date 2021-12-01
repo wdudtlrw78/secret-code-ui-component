955e13b8f133e4b1ae2424389ab51bf44130f201
momo <wdudtlrw78@gmail.com>
2021-12-01 22:29:19 +0900

    feat: scroll-top 강의 크로스브루아징

--- 5.scroll-top/1.js_1/main.js
+++ 5.scroll-top/1.js_1/main.js
@@ -42,25 +42,53 @@ const nav = document.querySelector('nav');
 
 // 두 번째 방법
 
-let oldValue = 0;
-window.addEventListener('scroll', function () {
-  const newValue =
-    window.scrollY ||
-    window.pageYOffset ||
-    document.documentElement.scrollTop ||
-    document.body.scrollTop;
+// let oldValue = 0;
+// window.addEventListener('scroll', function () {
+//   const newValue =
+//     window.scrollY ||
+//     window.pageYOffset ||
+//     document.documentElement.scrollTop ||
+//     document.body.scrollTop;
 
-  // 음수 : 스크롤 다운
-  if (oldValue - newValue < 0) {
-    nav.classList.add('active');
-  }
+//   // 음수 : 스크롤 다운
+//   if (oldValue - newValue < 0) {
+//     nav.classList.add('active');
+//   }
 
-  // 양수 : 스크롤 업
-  if (oldValue - newValue > 0) {
-    nav.classList.remove('active');
-  }
+//   // 양수 : 스크롤 업
+//   if (oldValue - newValue > 0) {
+//     nav.classList.remove('active');
+//   }
 
-  oldValue = newValue;
+//   oldValue = newValue;
 
-  console.log(oldValue, newValue);
-});
+//   console.log(oldValue, newValue);
+// });
+
+// 세 번째 방법
+/*
+window.addEventListener('wheel', mouseWheelEvent);
+window.addEventListener('DOMMouseScroll', mouseWheelEvent);
+
+function mouseWheelEvent(e) {
+    const delta = e.wheelDelta ? e.wheelDelta : -e.detail;
+    (delta < 0)
+        ? nav.classList.add('active')
+        : nav.classList.remove('active');
+}
+*/
+
+/*
+// refactoring
+const isFireFox = (navigator.userAgent.indexOf('Firefox') !== -1);
+const wheelEvt = isFireFox ? 'DOMMouseScroll' : 'wheel';
+
+window.addEventListener(wheelEvt, mouseWheelEvent);
+
+function mouseWheelEvent(e) {
+    const delta = e.wheelDelta ? e.wheelDelta : -e.detail;
+    (delta < 0)
+        ? nav.classList.add('active')
+        : nav.classList.remove('active');
+}
+*/
