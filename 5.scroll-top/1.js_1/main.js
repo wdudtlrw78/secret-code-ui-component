398f27d6428a0296e283a44b80a05dfaaf15c633
momo <wdudtlrw78@gmail.com>
2021-12-01 21:36:38 +0900

    feat: scroll-top basic

---
+++ 5.scroll-top/1.js_1/main.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('nav');
+
+// 첫 번째 방법
+// 스크롤 이벤트가 발생했을 때
+
+// 현재 스크롤 위치를 가져온다.
+
+// 스크롤 위치를 바탕으로 active 클래스를 추가하거나 제거한다.
+
+// Write JS Code Here!
+function handleScroll(e) {
+  const { scrollTop } = e.target.scrollingElement;
+
+  if (scrollTop === 0) {
+    nav.classList.remove('active');
+  } else {
+    nav.classList.add('active');
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
