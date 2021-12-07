c233c9e41f049602184448fa2eed55423ba0c3ca
momo <wdudtlrw78@gmail.com>
2021-12-07 22:54:58 +0900

    fix: stop-watch_js 강의

--- 7.stop-watch2/q1_js/index.js
+++ 7.stop-watch2/q1_js/index.js
@@ -1,62 +1,44 @@
-const min = document.querySelector('#min');
-const sec = document.querySelector('#sec');
-
-console.log(min.textContent, sec.textContent);
-
 var Timer = {
   isPending: true,
   currentSec: 0,
   currentMin: 0,
 
-  timerId: null,
-
   startTimer: function () {
-    // q1-1). isPending 이 false 일때만 setTimeout 을 재귀 호출하는 것을 만드세요.
-    // 60초가 되면 currentSec 을 0 으로, 분을 하나 증가 시킨다.
-    // 매 분초마다 시간을 html 에 적는다
-    console.log('start');
-    this.isPending = false;
-    if (this.isPending) return;
-    else {
-      this.timerId = setTimeout(() => {
-        this.startTimer();
-        this.currentSec += 1;
-
-        sec.textContent =
-          this.currentSec < 10 ? `0${this.currentSec}` : this.currentSec + '';
-
-        if (this.currentSec === 60) {
-          this.currentMin += 1;
-          this.currentSec = 0;
-
-          min.textContent =
-            this.currentMin < 10 ? `0${this.currentMin}` : this.currentMin + '';
-          sec.textContent = '00';
-        }
-      }, 1000);
+    if (!this.isPending) {
+      this.currentSec += 1;
+      if (this.currentSec == 60) {
+        this.currentSec = 0;
+        this.currentMin += 1;
+      }
+      document.querySelector('#min').innerHTML = ('0' + this.currentMin).slice(
+        -2
+      );
+      document.querySelector('#sec').innerHTML = ('0' + this.currentSec).slice(
+        -2
+      );
+      setTimeout('Timer.startTimer()', 1000);
     }
   },
 
   pauseTimer: function () {
     this.isPending = true;
-    clearTimeout(this.timerId);
   },
 
   resetTimer: function () {
     this.isPending = true;
-    // q1-2) 화면과 시간을 00 으로 초기화 한다.
-    clearTimeout(this.timerId);
     this.currentSec = 0;
     this.currentMin = 0;
-
-    sec.textContent = '00';
-    min.textContent = '00';
+    document.querySelector('#min').innerHTML = '00';
+    document.querySelector('#sec').innerHTML = '00';
   },
 };
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#start').addEventListener('click', function () {
-    // q1-3) 타이머 시작부분을 만든다.
-    Timer.startTimer();
+    // 시작버튼 연속클릭 방지
+    if (Timer.isPending) {
+      Timer.isPending = false;
+      Timer.startTimer();
+    }
   });
 
   document.querySelector('#pause').addEventListener('click', function () {
