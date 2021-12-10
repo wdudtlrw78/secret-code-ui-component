0c47b8cae02d5a5a19bbd4ae9dd17d307a238075
momo <wdudtlrw78@gmail.com>
2021-12-10 14:17:13 +0900

    feat: analog-clock_react

--- 8.analog-clock/1.js/index.js
+++ 8.analog-clock/1.js/index.js
@@ -1,15 +1,8 @@
-// 요구 사항은 다음과 같다.
-
-// 1. 시계의 시침(.hand.hour 요소), 분침(.hand.minute 요소), 초침(.hand.second 요소)을
-// 1초 간격으로 회전시켜 현재 시간을 표시한다.
-
 const renderTime = (() => {
   const $hourHand = document.querySelector('.hand.hour');
   const $minuteHand = document.querySelector('.hand.minute');
   const $secondHand = document.querySelector('.hand.second');
 
-  // 즉시실행 함수: 위의 3개는 1초당 렌더링이 될 필요가 없으므로 즉시실행함수를 사용해서 return 부분만 렌더링
-  // 즉 클로저를 이용하여 상위스코프 위의 변수들을 참조한다
   return () => {
     const now = new Date();
     const seconds = now.getSeconds();
