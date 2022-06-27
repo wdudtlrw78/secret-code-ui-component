fa33396063b21e5ad29a0f0127d75fabf20b5569
wdudtlrw78 <wdudtlrw78@gmail.com>
2022-06-27 22:37:25 +0900

    [Add] Add page nation with react

---
+++ 9.pagination/q3_react_redux/src/containers/CommentListContainer.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import CommentList from "../components/CommentList";
+import { getComments } from "../store/modules/comments";
+
+function CommentListContainer() {
+  const { data } = useSelector((state) => state.comments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    /* do something here */
+    dispatch( getComments({ page: 1 }) );
+  }, [dispatch]);
+
+  return <CommentList data={data} />;
+}
+
+export default CommentListContainer;
